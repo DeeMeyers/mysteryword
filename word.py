820760e5aa624c07afa028f8a6e0f79a01abae5e
DeeMeyers <justdmeyers@gmail.com>
2020-02-20 15:44:08 -0500

    set up more classes. start game works

--- word.py
+++ word.py
@@ -1,24 +1,54 @@
 from random import randint
 maxTurns = 8
+startingTurns = 0
+wordLengthEasy = [4,6]
+wordLengthNormal = [6,8]
+wordLengthHard = 8
 
 class Wordpick:
     def __init__(self, file):
         self.lineCount = 0
         self.word = ''
         self.intt = 0
-        # print('ive run')
+        # i will add a sort here for different difficulties
+        # if i can make that its own file i can use the same methods below
         with open(file, 'r') as f:
             for line in f:
                 self.lineCount += 1
-            # print(self.lineCount)
             self.intt = randint(0, (self.lineCount-1))
-            print(self.intt)
-        # with open(file, 'r') as f:
-            # print(f.read())
         with open(file) as f:
             self.word = f.readlines()
-            print(self.word[self.intt])
     def __str__(self):
         return self.word[self.intt]
 
-Wordpick('testwords.txt')
+class Game:
+    def __init__(self, file):
+        self.word = Wordpick('testwords.txt')
+        self.maxTurns = maxTurns
+        self.player = Player(self.word)
+        print("game ran")
+
+
+class Player:
+    def __init__(self, pickedWord):
+        self.word = pickedWord
+        self.turnCount = 0
+
+
+
+
+class GameStart:
+    def __init__(self):
+        start = input("Do you wanna (g)o or what?")
+        if start == "g":
+            Game('testwords.txt')
+        else:
+            print("booooooooooo!")
+            exit()
+
+
+
+
+
+
+
